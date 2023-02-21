46961a337a31566d945cc65eb584e128b2c78265
Timothy Wasike <wasiketimothylambert@gmail.com>
2023-02-21 13:25:33 +0300

    2
    Lunch Break

--- odoo/custom_addons/kmc_hospital/models/pathology.py
+++ odoo/custom_addons/kmc_hospital/models/pathology.py
@@ -5,8 +5,8 @@ class HospitalPathology(models.Model):
     _name = 'hospital.pathology'
     _description = 'Hospital Pathology'
 
-    name = fields.Char(string="Name", required=True)
-    code = fields.Char(string="Code")
+    name = fields.Char(string="Description/Name", required=True)
+    code = fields.Char(string="ICD-10 Code")
     category_id = fields.Many2one('hospital.pathology.category', string="Disease Category")
     line_ids = fields.One2many('hospital.pathology.group.member', 'disease_group_id', string="Group")
     chromosome = fields.Char(string="Affected Chromosome")
